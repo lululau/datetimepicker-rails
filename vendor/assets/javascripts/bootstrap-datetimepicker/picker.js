922c5fd08c1caeb3b5d6baa9906147058d2942b6
lululau <liuxiang921@gmail.com>
2013-12-06 02:49:20 +0800

    m

---
+++ vendor/assets/javascripts/bootstrap-datetimepicker/picker.js
@@ -0,0 +1,12 @@
+$('.datetimepicker').datetimepicker({
+   pickSeconds: false
+});
+
+$('.timepicker').datetimepicker({
+    pickDate: false,
+    pickSeconds: false
+});
+
+$('.datepicker').datetimepicker({
+    pickTime: false
+});
