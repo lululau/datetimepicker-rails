603b7d06c8d828a637e0a0d9859da9f4b24ee0d3
lululau <liuxiang921@gmail.com>
2013-12-06 20:39:50 +0800

    m

--- lib/generators/templates/inputs/date_picker_input.rb
+++ lib/generators/templates/inputs/date_picker_input.rb
@@ -1,7 +1,7 @@
 class DatePickerInput < SimpleForm::Inputs::StringInput
   def input
     value = object.send(attribute_name) if object.respond_to? attribute_name
-    display_pattern = I18n.t('datepicker.dformat', :default => 'YYYY-MM-DD')
+    display_pattern = I18n.t('datepicker.dformat', :default => '%Y-%m-%d')
     input_html_options[:value] ||= I18n.localize(value, :format => display_pattern) if value.present?
 
     input_html_options[:type] = 'text'
--- lib/generators/templates/inputs/datetime_picker_input.rb
+++ lib/generators/templates/inputs/datetime_picker_input.rb
@@ -1,7 +1,7 @@
 class DatetimePickerInput < SimpleForm::Inputs::StringInput
   def input
     value = object.send(attribute_name) if object.respond_to? attribute_name
-    display_pattern = I18n.t('datepicker.dformat', :default => 'YYYY-MM-DD') + ' ' + I18n.t('timepicker.dformat', :default => 'HH:mm')
+    display_pattern = I18n.t('datepicker.dformat', :default => '%Y-%m-%d') + ' ' + I18n.t('timepicker.dformat', :default => '%R')
     input_html_options[:value] ||= I18n.localize(value, :format => display_pattern) if value.present?
 
     input_html_options[:type] = 'text'
